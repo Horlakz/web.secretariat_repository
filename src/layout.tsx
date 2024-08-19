1365520e1c8a160d5049bd088a2d92e2f9d38011
Horlakz <officialhorlakz@gmail.com>
2024-08-19 09:59:26 +0100

    fix(app layout): adds margin auto

--- src/layout.tsx
+++ src/layout.tsx
@@ -3,7 +3,9 @@ import { FC } from "react";
 import { Children } from "./interface";
 
 const AppLayout: FC<Children> = ({ children }) => {
-  return <div className="min-h-screen max-w-screen-2xl">{children}</div>;
+  return (
+    <div className="min-h-screen max-w-screen-2xl mx-auto">{children}</div>
+  );
 };
 
 export default AppLayout;
