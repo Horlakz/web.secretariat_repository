67e2e1ae6ec315e7f3a6fdcff5d6f0e0fbcd99c7
Horlakz <officialhorlakz@gmail.com>
2024-08-19 08:49:16 +0100

    feat(/src/pages/): adds screen width constraint to app layout

--- src/layout.tsx
+++ src/layout.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 import { Children } from "./interface";
 
 const AppLayout: FC<Children> = ({ children }) => {
-  return <div className="min-h-screen">{children}</div>;
+  return <div className="min-h-screen max-w-screen-2xl">{children}</div>;
 };
 
 export default AppLayout;
