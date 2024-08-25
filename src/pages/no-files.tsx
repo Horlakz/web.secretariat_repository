7d7acbe2b8ddb3ee77592f02d9d8355bb8ed90da
Horlakz <officialhorlakz@gmail.com>
2024-08-25 06:56:17 +0100

    chore(no file component): adds upload file component to empty screen

--- src/pages/no-files.tsx
+++ src/pages/no-files.tsx
@@ -1,13 +1,26 @@
-import nofileUploaded from "@/assets/no-file-uploaded.png";
-import Button from "@/components/ui/button";
+import { useState } from "react";
 import { HiOutlinePlus } from "react-icons/hi2";
 
+import nofileUploaded from "@/assets/no-file-uploaded.png";
+import Button from "@/components/ui/button";
+import UploadFile from "./upload-file";
+
 function NoFiles() {
+  const [uploadFileVisibility, setUploadFileVisibility] = useState(false);
+
   return (
     <section className="flex-col-center gap-4">
+      <UploadFile
+        visibility={uploadFileVisibility}
+        setVisibility={() => setUploadFileVisibility(false)}
+      />
       <img src={nofileUploaded} alt="No file uploaded yet" />
       <p className="text-4xl font-medium">No file uploaded yet</p>
-      <Button icon={<HiOutlinePlus />} size="lg">
+      <Button
+        icon={<HiOutlinePlus />}
+        size="lg"
+        onClick={() => setUploadFileVisibility(true)}
+      >
         Upload file
       </Button>
     </section>
