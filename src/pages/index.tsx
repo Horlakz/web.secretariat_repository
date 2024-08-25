96c08da50a0187328781082ebfb7c9bed2954247
Horlakz <officialhorlakz@gmail.com>
2024-08-25 06:53:59 +0100

    chore(dashboard page): adds options dropdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+
 import { HiOutlinePlus } from "react-icons/hi2";
+import { RiDeleteBin6Line, RiShareForward2Fill } from "react-icons/ri";
+import { SlOptions } from "react-icons/sl";
 
 import Button from "@/components/ui/button";
 import { bytesToMegaBytes } from "@/utilities/common";
-import { useQuery } from "@tanstack/react-query";
-import { AxiosResponse } from "axios";
-import { useState } from "react";
-import { SlOptions } from "react-icons/sl";
 import { IFileResponsePayload } from "./interface";
 import NoFiles from "./no-files";
+import Share from "./share";
 import UploadFile from "./upload-file";
 
 const HomePage = () => {
-  const [visibility, setVisibility] = useState(false);
+  const [uploadFileVisibility, setUploadFileVisibility] = useState(false);
+  const [shareVisibility, setShareVisibility] = useState(false);
+  const [show, setShow] = useState<null | number>(null);
+  const [fileId, setFileId] = useState<string>("");
+  const ref = useRef<HTMLTableCellElement>(null);
+
+  function handleOutsideClick(event: MouseEvent) {
+    // If the click is outside the dropdown, close it
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      setShow(null);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
 
   const query = useQuery<AxiosResponse<IFileResponsePayload>>({
     queryKey: ["file"],
@@ -23,14 +45,19 @@ const HomePage = () => {
   return (
     <div className="p-6">
       <UploadFile
-        visibility={visibility}
-        setVisibility={() => setVisibility(false)}
+        visibility={uploadFileVisibility}
+        setVisibility={() => setUploadFileVisibility(false)}
+      />
+      <Share
+        fileId={fileId}
+        visibility={shareVisibility}
+        setVisibility={() => setShareVisibility(false)}
       />
       <div className="w-full flex justify-start">
         <Button
           icon={<HiOutlinePlus />}
           size="lg"
-          onClick={() => setVisibility(true)}
+          onClick={() => setUploadFileVisibility(true)}
         >
           Upload file
         </Button>
@@ -79,10 +106,52 @@ const HomePage = () => {
                   <td className="border-b border-blue-700 py-3.5">
                     {new Date(row.created_at).toLocaleDateString()}
                   </td>
-                  <td className="border-b border-blue-700 py-3.5">
-                    <Button variant="ghost">
+                  <td
+                    ref={ref}
+                    className="relative border-b border-blue-700 py-3.5"
+                  >
+                    <Button
+                      variant="ghost"
+                      onClick={() => {
+                        setShow(index);
+                        setFileId(row.id);
+                      }}
+                    >
                       <SlOptions />
                     </Button>
+
+                    <AnimatePresence>
+                      {show === index && (
+                        <motion.div
+                          className="absolute top-10 right-0 bg-white shadow-md rounded-lg p-2 z-10"
+                          initial={{ opacity: 0, y: -10 }}
+                          animate={{ opacity: 1, y: 0 }}
+                          exit={{ opacity: 0, y: -10 }}
+                          transition={{ duration: 0.2 }}
+                        >
+                          <ul className="flex flex-col gap-2 px-4">
+                            <li>
+                              <Button
+                                variant="ghost"
+                                icon={<RiShareForward2Fill />}
+                                onClick={() => setShareVisibility(true)}
+                              >
+                                Share
+                              </Button>
+                            </li>
+                            <li>
+                              <Button
+                                variant="ghost"
+                                icon={<RiDeleteBin6Line />}
+                                colorScheme="danger"
+                              >
+                                Delete
+                              </Button>
+                            </li>
+                          </ul>
+                        </motion.div>
+                      )}
+                    </AnimatePresence>
                   </td>
                 </tr>
               ))}
