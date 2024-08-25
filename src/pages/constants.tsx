89d6cbde503ffa29893ed0a7355f1c98fed593f3
Horlakz <officialhorlakz@gmail.com>
2024-08-25 07:26:48 +0100

    chore(/src/pages/): removes messages and renames community to levels

--- src/pages/constants.tsx
+++ src/pages/constants.tsx
@@ -1,12 +1,11 @@
-import { BsBell, BsChatText } from "react-icons/bs";
+import { BsBell } from "react-icons/bs";
 import { HiOutlineUsers } from "react-icons/hi2";
 import { IoSettingsOutline } from "react-icons/io5";
 import { PiSquaresFourLight } from "react-icons/pi";
 
 export const menus = [
   { icon: PiSquaresFourLight, label: "Dashboard" },
-  { icon: BsChatText, label: "Messages" },
-  { icon: HiOutlineUsers, label: "Community" },
+  { icon: HiOutlineUsers, label: "Levels" },
   { icon: BsBell, label: "Notifications" },
   { icon: IoSettingsOutline, label: "Settings" },
 ];
