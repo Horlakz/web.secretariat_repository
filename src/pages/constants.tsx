9231b44e971841ddf60653ab5d02b8f0e8c35209
Horlakz <officialhorlakz@gmail.com>
2024-08-26 07:17:42 +0100

    ehancment(/src/pages/): removes notification from side bar

--- src/pages/constants.tsx
+++ src/pages/constants.tsx
@@ -1,4 +1,3 @@
-import { BsBell } from "react-icons/bs";
 import { HiOutlineUsers } from "react-icons/hi2";
 import { IoSettingsOutline } from "react-icons/io5";
 import { PiSquaresFourLight } from "react-icons/pi";
@@ -6,6 +5,5 @@ import { PiSquaresFourLight } from "react-icons/pi";
 export const menus = [
   { icon: PiSquaresFourLight, label: "Dashboard" },
   { icon: HiOutlineUsers, label: "Levels" },
-  { icon: BsBell, label: "Notifications" },
   { icon: IoSettingsOutline, label: "Settings" },
 ];
