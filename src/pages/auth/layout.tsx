31d5d608d93f866baa7b6ae7a4474602ccea709a
Horlakz <officialhorlakz@gmail.com>
2024-08-29 13:01:39 +0100

    feat(/src/pages/auth/layout/): adds responsiveness to page

--- src/pages/auth/layout.tsx
+++ src/pages/auth/layout.tsx
@@ -17,7 +17,7 @@ const AuthLayout = () => {
 
   return (
     <div className="flex items-center w-full h-screen">
-      <section className="w-full h-full px-20 bg-[#D1F1FF] flex-col-center text-center">
+      <section className="w-full h-full px-20 bg-[#D1F1FF] hidden sm:flex flex-col items-center justify-center text-center">
         <Button
           variant="ghost"
           as="img"
@@ -25,7 +25,7 @@ const AuthLayout = () => {
           onClick={() => router.goTo("/")}
         />
         <h1 className="text-5xl font-inter text-secondary pb-6">
-          Secretariat Repositary
+          Secretariat Repository
         </h1>
         <p className="font-medium text-[#0049A8] text-2xl">
           Simplify administration and management with secure access, easy
@@ -34,7 +34,17 @@ const AuthLayout = () => {
       </section>
 
       <section className="w-full grid items-center">
-        <div className="px-36">
+        <div className="sm:hidden block text-center mb-10">
+          <Button
+            variant="ghost"
+            as="img"
+            src={logo}
+            onClick={() => router.goTo("/")}
+            className="w-20 h-20 mx-auto"
+          />
+          <h1 className="text-4xl">Secretariat Repository</h1>
+        </div>
+        <div className="sm:px-36 px-10">
           <Outlet />
         </div>
       </section>
