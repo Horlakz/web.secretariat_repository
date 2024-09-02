eca73130d82e88533a98cf9636893670e83a3e14
Horlakz <officialhorlakz@gmail.com>
2024-09-02 07:01:48 +0100

    enhancement(/src/pages/): adds navbar active state

--- src/pages/sidebar.tsx
+++ src/pages/sidebar.tsx
@@ -2,6 +2,7 @@ import logo from "@/assets/logo.png";
 
 import Button from "@/components/ui/button";
 import { useRouter } from "@/router/router.hook";
+import classNames from "classnames";
 import { menus } from "./constants";
 
 const SideBar = () => {
@@ -17,14 +18,23 @@ const SideBar = () => {
         onClick={() => router.goTo("/")}
       />
 
-      <ul className="flex flex-col gap-10">
+      <ul className="flex flex-col gap-6">
         {menus.map((item, i) => (
-          <li
-            key={i}
-            className="flex justify-start items-center gap-3 cursor-pointer"
-          >
-            <item.icon size={24} />
-            <span className="text-lg">{item.label}</span>
+          <li key={i}>
+            <Button
+              variant="ghost"
+              icon={<item.icon size={24} />}
+              onClick={() => router.goTo(item.path)}
+              className={classNames(
+                "px-4 py-2.5 hover:bg-[#D6EAF8] hover:text-[#2B6CB0]",
+                {
+                  "bg-[#d4ebf7] text-[#2B6CB0] hover:bg-[#D6EAF8] hover:text-[#2B6CB0]":
+                    router.pathname === item.path,
+                }
+              )}
+            >
+              <span className="text-lg">{item.label}</span>
+            </Button>
           </li>
         ))}
       </ul>
