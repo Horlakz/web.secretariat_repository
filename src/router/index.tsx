de6420a26409e9a561d3b74cc34803740d273e21
Horlakz <officialhorlakz@gmail.com>
2024-09-02 07:23:38 +0100

    refactor(/src/router/): removes files and creates separate files for routers

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import ErrorPage from "@/pages/404";
-import AuthLayout from "@/pages/auth/layout";
-import BaseLayout from "@/pages/layout";
-
-import { authRoutes } from "@/router/auth.route";
-import { layoutRoutes } from "@/router/layout.route";
-
-const routes = [
-  { path: "*", element: <ErrorPage /> },
-  { path: "/", element: <BaseLayout />, children: layoutRoutes },
-  { path: "/", element: <AuthLayout />, children: authRoutes },
-];
+import routes from "./routes";
 
 const router = createBrowserRouter(routes);
 
