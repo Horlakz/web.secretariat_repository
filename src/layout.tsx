621c48a7fc6c7fd9720431f5b896e5aead89f052
Horlakz <officialhorlakz@gmail.com>
2024-09-02 06:49:06 +0100

    enhancement(/src/): removes max-w-screen-2xl style from app layout

--- src/layout.tsx
+++ src/layout.tsx
@@ -3,9 +3,7 @@ import { FC } from "react";
 import { Children } from "./interface";
 
 const AppLayout: FC<Children> = ({ children }) => {
-  return (
-    <div className="min-h-screen max-w-screen-2xl mx-auto">{children}</div>
-  );
+  return <div className="min-h-screen">{children}</div>;
 };
 
 export default AppLayout;
